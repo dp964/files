54a4a44d2eeb26d765315c1bccdaab18eaef348c
dp964 <dp964@users.noreply.github.com>
2019-08-22 16:15:31 +1000

    Update icecream.js

--- icecream.js
+++ icecream.js
@@ -3,8 +3,8 @@ function loadx()
 	var scoop=getParameterByName('scoop', "1");
 	var type=getParameterByName('type', "Cup");
 	var flavour=getParameterByName('flavour', "Vanilla");
-	var price=getParameterByName('price', "6.50");
-	var notes=getParameterByName('notes', "0000")	
+	var price=getParameterByName('price', "$6.50");
+	var notes=getParameterByName('notes', "none")	
 	document.write("<h2>Icecream Entry</h2>");
 	document.write("Number of Scoops:" + scoop+"<br>");
 	document.write("Cup or Cone:" + type+"<br>");
