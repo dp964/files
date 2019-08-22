ce956947469c4f2260b38c9f3cf9a53dccd9da50
dp964 <dp964@users.noreply.github.com>
2019-08-22 16:15:43 +1000

    Update icecream.js

--- icecream.js
+++ icecream.js
@@ -6,11 +6,11 @@ function loadx()
 	var price=getParameterByName('price', "$6.50");
 	var notes=getParameterByName('notes', "none")	
 	document.write("<h2>Icecream Entry</h2>");
-	document.write("Number of Scoops:" + scoop+"<br>");
-	document.write("Cup or Cone:" + type+"<br>");
-	document.write("Flavours:" + flavour+"<br>");
-	document.write("Price:" + price+"<br>");
-	document.write("Notes:" + notes+"<br>");		
+	document.write("Number of Scoops: " + scoop+"<br>");
+	document.write("Cup or Cone: " + type+"<br>");
+	document.write("Flavours: " + flavour+"<br>");
+	document.write("Price: " + price+"<br>");
+	document.write("Notes: " + notes+"<br>");		
 }
 function getParameterByName(name, defaultx, url) {
     if (!url) url = window.location.href;
